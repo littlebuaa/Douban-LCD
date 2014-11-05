dbac54c3e7c3133be99638c4c5c0982b311464d4
TuoLI <tuo.de.li@gmail.com>
2014-11-05 20:46:04 +0100

    Create DoubanFM class

--- DoubanFM.py
+++ DoubanFM.py
@@ -8,70 +8,85 @@ import urllib
 import subprocess
 import time
 
-
-url='http://www.douban.com/j/app/login'
-post_data = {
-'app_name' : 'radio_desktop_win',
-'version' : 100,
-'email' : 'lituobuaa@126.com',
-'password' : '35022426abcd',
-}
-# create Post Data.
-
-post_data = urllib.urlencode(post_data)
-req = urllib2.Request(url, post_data)
-result = urllib2.urlopen(req)
-text = result.read()
-
-jsdata = json.loads(text)
-
-token = jsdata['token']
-user_id = jsdata['user_id']
-expire = jsdata['expire']
-
-#x = [token,user_id,expire]
+class Douban(object):
 
 
-For_song = {
-'app_name' : 'radio_desktop_win',
-'version' : '100',
-'user_id': user_id,
-'token':token,
-'expire' :expire,
-'channel':'-3',
-'type':'n',
-    }
+        def __init__(self,email,psword):
+                '''Create the login object'''
+                
+                self.url='http://www.douban.com/j/app/login' 
+                self.post_data = {
+                        'app_name' : 'radio_desktop_win',
+                        'version' : 100,
+                        'email' : email, #'lituobuaa@126.com'
+                        'password' : psword  #'35022426abcd',
+                                }
+                self.resLogin={}
+        # create Post Data.
+        def login(self):
+                
+                post_data = urllib.urlencode(self.post_data)
+                req = urllib2.Request(self.url, post_data)
+                result = urllib2.urlopen(req)
+                text = result.read()
+                jsdata = json.loads(text)
 
-url = 'http://www.douban.com/j/app/radio/people'
-post_data = urllib.urlencode(For_song)
-url = url +'?'+ post_data
-req = urllib2.Request(url)
+                self.resLogin ={
+                          'token': jsdata['token'],
+                        'user_id':jsdata['user_id'],
+                        'expire' :jsdata['expire']
+                        }
+                print jsdata['user_id']
+                print "Get the Token"
 
-result = urllib2.urlopen(req)
-text = result.read()
+        #x = [token,user_id,expire]
 
-jsdata = json.loads(text)
+        def getSong(self,nbChannel,listype):                
 
-#print jsdata['err']
-#print jsdata['song']  
+                # nbChannel is No. of FM Channel,eg, -3 is personel channel.
+                # listype is a char, eg 'n' represents a new list
 
-content=jsdata['song']  
-playlist = ''
+                For_song = {
+                'app_name' : 'radio_desktop_win',
+                'version' : '100',
+                'channel': nbChannel,
+                'type': listype
+                }
+                newPost = self.resLogin.copy()
+                newPost.update(For_song)
+                
+                url = 'http://www.douban.com/j/app/radio/people'
+                post_data = urllib.urlencode(newPost)
+                url = url +'?'+ post_data
+                req = urllib2.Request(url)
 
-for s in content:
-        playlist = playlist  + s['url']+ "\n"
-#print "字符串长度：",len(content) 
-fp=open("song.txt","w")  
+                result = urllib2.urlopen(req)
+                text = result.read()
 
-fp.write(playlist)  
-fp.close()
+                jsdata = json.loads(text)
+
+                #print jsdata['err']
+                #print jsdata['song']  
+
+                content=jsdata['song']  
+                playlist = ''
+
+                for s in content:
+                        playlist = playlist  + s['url']+ "\n"
+
+                #print "字符串长度：",len(content) 
+                fp=open("song.txt","w")  
+                fp.write(playlist)  
+                fp.close()
+
+                return content 
 
 
-Player = subprocess.Popen(['mpg123','-C','-@',"song.txt"],stdout= subprocess.PIPE)
-Radio = subprocess.Popen(['sudo','PiFM/pifm','-','88.9','22050'],stdin = Player.stdout)
+#Player = subprocess.Popen(['mpg123','-C','-@',"song.txt"],stdout= subprocess.PIPE)
+#Radio = subprocess.Popen(['sudo','PiFM/pifm','-','88.9','22050'],stdin = Player.stdout)
 #Player = subprocess.Popen(['mpg123','-C','-@',"song.txt"])
-time.sleep(content[0]['length'])
-Player.kill()
-Radio.kill()
+#time.sleep(content[0]['length'])
+#Player.kill()
+#Radio.kill()
 
 
